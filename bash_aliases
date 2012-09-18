08a1fae681242457172b1c03da97f26122a1dbe8
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2012-09-17 21:29:03 -0400

    Adds Commonly Used Aliases for "ls"
    sl, ll, and la aliases are defined. These changes are placed in
    bash_aliases, which the installer is updated to install.

---
+++ bash_aliases
@@ -0,0 +1,4 @@
+# aliases for ls and variants of ls
+alias sl="ls"
+alias ll="ls -l"
+alias la="ls -A"
--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # install our bashrc to $HOME
 cp -f bashrc $HOME/.bashrc
+cp -f bash_aliases $HOME/.bash_aliases
